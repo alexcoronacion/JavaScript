b9a401ebb732f7ca88d9c78cc8a2608c08a9538b
alexcoronacion <alex.coronacion2022@gmail.com>
2022-07-21 22:54:12 -0500

    commit01

---
+++ examenFinal/ejercicio1.js
@@ -0,0 +1,34 @@
+
+
+function distingueLetras (argumentos) {
+
+    var x = 0;
+    var y = 0;
+    var regex=/(\d+)/g;
+    
+   
+    // recorro el string
+    for (var i = 0; i < argumentos.length; i++) {
+      var charCode = argumentos[i].charCodeAt();
+      
+      // si pertenece a las mayúsculas
+      if (charCode >= 65 && charCode <= 90) {
+        x+=argumentos.charAt(i);
+      }
+      // si pertenece a las minúsculas
+      else if (charCode >= 97 && charCode <= 122) {
+        y+=argumentos.charAt(i);
+      }
+     
+    }
+    console.log("Cadena de Números: "+argumentos.match(regex));
+    console.log(`Cadena de letras MAYUSCULAS:  ${x} \n Cadena de letras MINUSCULAS:  ${y} `)
+    
+
+  }
+
+//}
+
+var cadena;
+cadena=inputs('Ingrese dato: ');
+distingueLetras(cadena);
