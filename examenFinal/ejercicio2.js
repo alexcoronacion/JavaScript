c5ea02f0e9e5d7f3ee410e672e8e6df976756ea6
alexcoronacion <alex.coronacion2022@gmail.com>
2022-07-22 10:29:02 -0500

    comit2

--- examenFinal/ejercicio2.js
+++ examenFinal/ejercicio2.js
@@ -1,8 +1,5 @@
-let text = "añlkjgasñlkjdñalsknñaslkjfd";
-console.log(text.slice(7));
-console.log(text.substring(7,13));
-console.log(text.substr(7,6));
-console.log(text.replace("a","Z"));
 
-let text2="Hola Mundo!";
-console.log(text2.toUpperCase());
+function agregarElemento(){
+    let dato
+}
+
